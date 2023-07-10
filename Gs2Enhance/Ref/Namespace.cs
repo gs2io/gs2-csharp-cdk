362f3f3d149e3b1b080f14f772df0f4d9a47f836
gs2-ci <ci@gs2.io>
2023-07-10 14:37:04 +0000

    170d75f26c41b507b86a77ccce0186f1e50223bd

--- Gs2Enhance/Ref/Namespace.cs
+++ Gs2Enhance/Ref/Namespace.cs
@@ -12,8 +12,6 @@
  * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
  * express or implied. See the License for the specific language governing
  * permissions and limitations under the License.
- *
- * deny overwrite
  */
 using System.Collections.Generic;
 using System.Linq;
@@ -46,8 +44,8 @@ namespace Gs2Cdk.Gs2Enhance.Ref
         public CreateProgressByUserId CreateProgress(
             string rateName,
             string targetItemSetId,
+            Material[] materials,
             bool? force,
-            Material[] materials = null,
             string userId = "#{userId}"
         ){
             return (new CreateProgressByUserId(
