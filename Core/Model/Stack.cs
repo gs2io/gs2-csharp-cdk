ec4a2cf865de71584a0229b91506715309781435
gs2-ci <ci@gs2.io>
2023-10-26 03:14:14 +0000

    de69eaa56d354f2e31740cbc31af865be84797aa

--- Core/Model/Stack.cs
+++ Core/Model/Stack.cs
@@ -43,6 +43,7 @@ namespace Gs2Cdk.Core.Model
         public string Yaml() {
             var serializer = new SerializerBuilder()
                 .DisableAliases()
+                .WithQuotingNecessaryStrings()
                 .WithNamingConvention(CamelCaseNamingConvention.Instance)
                 .Build();
             return new Regex("['\"]!(.*) (.*)['\"]").Replace(serializer.Serialize(Template()), "!$1 $2");
