9a74374bc4aa33debfb9a307592f5d205d6dcb4f
gs2-ci <ci@gs2.io>
2024-09-23 22:54:52 +0000

    6afb573a776223b1cd8bbcdb48df55a3e02488b2

---
+++ Gs2Mission/StampSheet/BatchReceiveByUserId.cs
@@ -0,0 +1,129 @@
+/*
+ * Copyright 2016- Game Server Services, Inc. or its affiliates. All Rights
+ * Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License").
+ * You may not use this file except in compliance with the License.
+ * A copy of the License is located at
+ *
+ *  http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * or in the "license" file accompanying this file. This file is distributed
+ * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either
+ * express or implied. See the License for the specific language governing
+ * permissions and limitations under the License.
+ */
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+using Gs2Cdk.Core.Model;
+using Gs2Cdk.Gs2Mission.Model;
+
+namespace Gs2Cdk.Gs2Mission.StampSheet
+{
+    public class BatchReceiveByUserId : ConsumeAction {
+        private string namespaceName;
+        private string missionGroupName;
+        private string userId;
+        private string[] missionTaskNames;
+        private string timeOffsetToken;
+
+
+        public BatchReceiveByUserId(
+            string namespaceName,
+            string missionGroupName,
+            string[] missionTaskNames,
+            string timeOffsetToken = null,
+            string userId = "#{userId}"
+        ){
+
+            this.namespaceName = namespaceName;
+            this.missionGroupName = missionGroupName;
+            this.missionTaskNames = missionTaskNames;
+            this.timeOffsetToken = timeOffsetToken;
+            this.userId = userId;
+        }
+
+        public override Dictionary<string, object> Request(
+        ){
+            var properties = new Dictionary<string, object>();
+
+            if (this.namespaceName != null) {
+                properties["namespaceName"] = this.namespaceName;
+            }
+            if (this.missionGroupName != null) {
+                properties["missionGroupName"] = this.missionGroupName;
+            }
+            if (this.userId != null) {
+                properties["userId"] = this.userId;
+            }
+            if (this.missionTaskNames != null) {
+                properties["missionTaskNames"] = this.missionTaskNames;
+            }
+            if (this.timeOffsetToken != null) {
+                properties["timeOffsetToken"] = this.timeOffsetToken;
+            }
+
+            return properties;
+        }
+
+        public static BatchReceiveByUserId FromProperties(Dictionary<string, object> properties) {
+            try {
+                return new BatchReceiveByUserId(
+                    (string)properties["namespaceName"],
+                    (string)properties["missionGroupName"],
+                    new Func<string[]>(() =>
+                    {
+                        return properties["missionTaskNames"] switch {
+                            string[] v => v.ToArray(),
+                            List<string> v => v.ToArray(),
+                            object[] v => v.Select(v2 => v2?.ToString()).ToArray(),
+                            { } v => new []{ v.ToString() },
+                            _ => null
+                        };
+                    })(),
+                    new Func<string>(() =>
+                    {
+                        return properties.TryGetValue("timeOffsetToken", out var timeOffsetToken) ? timeOffsetToken as string : null;
+                    })(),
+                    new Func<string>(() =>
+                    {
+                        return properties.TryGetValue("userId", out var userId) ? userId as string : "#{userId}";
+                    })()
+                );
+            } catch (Exception e) when (e is FormatException || e is OverflowException) {
+                return new BatchReceiveByUserId(
+                    properties["namespaceName"].ToString(),
+                    properties["missionGroupName"].ToString(),
+                    new Func<string[]>(() =>
+                    {
+                        return properties["missionTaskNames"] switch {
+                            string[] v => v.ToArray(),
+                            List<string> v => v.ToArray(),
+                            object[] v => v.Select(v2 => v2?.ToString()).ToArray(),
+                            { } v => new []{ v.ToString() },
+                            _ => null
+                        };
+                    })(),
+                    new Func<string>(() =>
+                    {
+                        return properties.TryGetValue("timeOffsetToken", out var timeOffsetToken) ? timeOffsetToken.ToString() : null;
+                    })(),
+                    new Func<string>(() =>
+                    {
+                        return properties.TryGetValue("userId", out var userId) ? userId as string : "#{userId}";
+                    })()
+                );
+            }
+        }
+
+        public override string Action() {
+            return "Gs2Mission:BatchReceiveByUserId";
+        }
+
+        public static string StaticAction() {
+            return "Gs2Mission:BatchReceiveByUserId";
+        }
+    }
+}
